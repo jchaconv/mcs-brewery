dfad4482a0131a0fddefd4ab404ebfa0146483a4
jchaconv <jcaaron.chacon@outlook.com>
2021-02-16 23:20:16 -0500

    Added initial files to the project.

---
+++ src/main/java/expert/springframework/mcsbrewery/McsBreweryApplication.java
@@ -0,0 +1,13 @@
+package expert.springframework.mcsbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expert/springframework/mcsbrewery/McsBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.springframework.mcsbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
