9b8b7e31b5519b4eb51186c88dd5d884da9e9dd6
jchaconv <jcaaron.chacon@outlook.com>
2021-02-20 17:13:57 -0500

    added mvc annotations for beer management

--- src/main/java/expert/springframework/mcsbrewery/services/BeerService.java
+++ src/main/java/expert/springframework/mcsbrewery/services/BeerService.java
@@ -5,7 +5,12 @@ import expert.springframework.mcsbrewery.web.model.BeerDto;
 import java.util.UUID;
 
 public interface BeerService {
+
     BeerDto getBeerById(UUID beerId);
 
     BeerDto saveNewBeer(BeerDto beerDto);
+
+    void updateBeer(UUID beerId, BeerDto beerDto);
+
+    void deleteById(UUID beerId);
 }
--- src/main/java/expert/springframework/mcsbrewery/services/BeerServiceImpl.java
+++ src/main/java/expert/springframework/mcsbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package expert.springframework.mcsbrewery.services;
 
 import expert.springframework.mcsbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -19,5 +21,15 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto saveNewBeer(BeerDto beerDto) {
         return BeerDto.builder().id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //todo impl
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer .... ");
     }
 }
