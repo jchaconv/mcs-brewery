9a2092cf44a3e4ec0174dcfa2232c72b089aa82b
jchaconv <jcaaron.chacon@outlook.com>
2021-02-23 16:13:33 -0500

    Adding MVC Controller Advice. It's not necessary to have duplicated code in different classes.

--- src/main/java/expert/springframework/mcsbrewery/web/controller/CustomerController.java
+++ src/main/java/expert/springframework/mcsbrewery/web/controller/CustomerController.java
@@ -54,14 +54,6 @@ public class CustomerController {
     }
 
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath() + ":" + constraintViolation.getMessage());
-        });
 
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 
 }
