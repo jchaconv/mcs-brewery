10bad1b8331333c1cc8f7b47bb338fa25f468676
jchaconv <jcaaron.chacon@outlook.com>
2021-02-23 23:02:40 -0500

    added DateMapper

--- src/main/java/expert/springframework/mcsbrewery/web/mappers/BeerMapper.java
+++ src/main/java/expert/springframework/mcsbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import expert.springframework.mcsbrewery.domain.Beer;
 import expert.springframework.mcsbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
